2c11ea3dbf83162e5ecc7221e78235f989e20e52
Yannik Stahl <yannik.stahl@gmx.net>
2023-06-01 18:54:13 +0200

    added python files for discord bot.

---
+++ main.py
@@ -0,0 +1,48 @@
+import os, re, discord
+from discord.ext import commands
+from discord import app_commands
+
+
+DISCORD_TOKEN = "MTExMzUwODEyNzQzNDg3OTAwNg.Gh_0tm.owyaW3Vhdb1Brl4kr_fHiBbtU9qx2Q5ggYWtwQ"
+
+client = discord.Client(intents=discord.Intents.all())
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="setpayout",
+             description="This command gives you a Role.")
+async def setpayout(interaction: discord.Interaction, role: discord.Role):
+  member = interaction.user
+  try:
+    await member.add_roles(role)
+  except Exception as e:
+    print(f"Error {e}  occurd while trying to add that Role.")
+  await interaction.response.send_message(
+    f"Successfully added Role '{role.name}' to User {interaction.user.mention}!",
+    ephemeral=True)
+
+
+@tree.command(name="resetpayout",
+             description="This command removes a Role from you.")
+async def resetpayout(interaction: discord.Interaction, role: discord.Role):
+  member = interaction.user
+  try:
+    await member.remove_roles(role)
+  except Exception as e:
+    print(f"Error {e}  occurd while trying to remove that Role.")
+  await interaction.response.send_message(
+    f"Successfully removed Role '{role.name}' from User {interaction.user.mention}!",
+    ephemeral=True)
+
+
+@client.event
+async def on_ready():
+  print(f"{client.user} has connected to Discord!")
+  try:
+    await tree.sync()    
+    print(f"Synced successfully!")
+  except Exception as e:
+    print(f"Error syncing commands: {e}")
+
+
+client.run(DISCORD_TOKEN)
